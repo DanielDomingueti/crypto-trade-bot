2628b590c9cc7ba195dbcd31c744e4dcaf79075a
Daniel Pires Domingueti <danieldomingueti@hotmail.com>
2022-08-21 10:54:45 -0300

    feat: initializing project

---
+++ TradeBot/src/main/java/com/domingueti/tradebot/TradeBotApplication.java
@@ -0,0 +1,13 @@
+package com.domingueti.tradebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeBotApplication.class, args);
+	}
+
+}
---
+++ TradeBot/src/test/java/com/domingueti/tradebot/TradeBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domingueti.tradebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
